079051939c3313b238aa937141d325989281d20f
Ridham Diyora <ridhamdiyora7@gmail.com>
2023-03-30 16:19:17 +0530

    commit

--- addOverallSentimentResult.py
+++ addOverallSentimentResult.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine, func
+from datetime import datetime
+from tables import SymbolConfig, ModelDetails, SentimentResult, StockSentimentResults
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime, timedelta
-from scraper import SymbolConfig, ModelDetails, SentimentResult, StockSentimentResults
 
 # Set up database connection
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/analysis')
@@ -36,28 +36,43 @@ for stock_id in stock_ids:
                 negative_count += 1
             else:
                 neutral_count += 1
-        
+
+        total_count = positive_count+negative_count+neutral_count
+
         # Determine the overall sentiment based on the counts
-        if positive_count > negative_count and positive_count > neutral_count:
+        if positive_count == max(positive_count, neutral_count, negative_count):
             overall_sentiment = 'positive'
-        elif negative_count > positive_count and negative_count > neutral_count:
+            overall_score = positive_count/total_count
+        elif negative_count == max(positive_count, neutral_count, negative_count):
             overall_sentiment = 'negative'
+            overall_score = negative_count/total_count
         else:
             overall_sentiment = 'neutral'
-        
-        # Calculate the final result based on the overall sentiment and sentiment scores
-        sentiment_scores = [sentiment_score for sentiment_result, sentiment_score in sentiment_results]
-        if overall_sentiment == 'positive':
-            final_result = sum(sentiment_scores) / len(sentiment_scores)
-        elif overall_sentiment == 'negative':
-            final_result = -sum(sentiment_scores) / len(sentiment_scores)
-        else:
-            final_result = 0.0
+            overall_score = neutral_count/total_count
+
+        # # Determine the overall sentiment based on the counts
+        # if positive_count > negative_count and positive_count > neutral_count:
+        #     overall_sentiment = 'positive'
+        # elif negative_count > positive_count and negative_count > neutral_count:
+        #     overall_sentiment = 'negative'
+        # else:
+        #     overall_sentiment = 'neutral'
+        #
+        # # Calculate the final result based on the overall sentiment and sentiment scores
+        # sentiment_scores = [sentiment_score for sentiment_result, sentiment_score in sentiment_results]
+        # if overall_sentiment == 'positive':
+        #     final_result = sum(sentiment_scores) / len(sentiment_scores)
+        # elif overall_sentiment == 'negative':
+        #     final_result = -sum(sentiment_scores) / len(sentiment_scores)
+        # else:
+        #     final_result = 0.0
         
         # Create a new record in the stock_sentiment_results table
         stock_sentiment_result = StockSentimentResults(
             stock_id=stock_id,
-            final_result=final_result,
+            model_id = model_id,
+            overall_sentiment = overall_sentiment,
+            overall_score = overall_score,
             time_from=datetime(2023, 3, 20, 0, 0, 0),
             time_to=datetime(2023, 3, 30, 0, 0, 0),
         )
@@ -65,3 +80,74 @@ for stock_id in stock_ids:
     
     session.commit()
 session.commit()
+
+
+
+
+# from sqlalchemy import create_engine, func
+# from sqlalchemy.orm import sessionmaker
+# from datetime import datetime, timedelta
+# from scraper import SymbolConfig, ModelDetails, SentimentResult, StockSentimentResults
+
+# # Set up database connection
+# engine = create_engine('postgresql://postgres:postgres@localhost:5432/analysis')
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+# # Get distinct stock IDs
+# stock_ids = [id for (id,) in session.query(SymbolConfig.id).distinct()]
+
+# # Get distinct model IDs
+# model_ids = [id for (id,) in session.query(ModelDetails.id).distinct()]
+
+# # Define threshold value
+# threshold = 0.7
+
+# # Loop over each stock and model to calculate the sentiment
+# for stock_id in stock_ids:
+#     for model_id in model_ids:
+#         # Get all sentiment results for the stock and model
+#         sentiment_results = session.query(SentimentResult.sentiment_result, SentimentResult.sentiment_score)\
+#             .filter(SentimentResult.symbol_config_id == stock_id, SentimentResult.model_name_id == model_id)\
+#             .all()
+        
+#         # Calculate the counts for each sentiment result
+#         positive_count = 0
+#         negative_count = 0
+#         neutral_count = 0
+#         for sentiment_result, sentiment_score in sentiment_results:
+#             if sentiment_result == 'positive' and sentiment_score >= threshold:
+#                 positive_count += 1
+#             elif sentiment_result == 'negative' and sentiment_score >= threshold:
+#                 negative_count += 1
+#             else:
+#                 neutral_count += 1
+        
+#         # Determine the overall sentiment based on the counts
+#         if positive_count > negative_count and positive_count > neutral_count:
+#             overall_sentiment = 'positive'
+#         elif negative_count > positive_count and negative_count > neutral_count:
+#             overall_sentiment = 'negative'
+#         else:
+#             overall_sentiment = 'neutral'
+        
+#         # Calculate the final result based on the overall sentiment and sentiment scores
+#         sentiment_scores = [sentiment_score for sentiment_result, sentiment_score in sentiment_results]
+#         if overall_sentiment == 'positive':
+#             final_result = sum(sentiment_scores) / len(sentiment_scores)
+#         elif overall_sentiment == 'negative':
+#             final_result = -sum(sentiment_scores) / len(sentiment_scores)
+#         else:
+#             final_result = 0.0
+        
+#         # Create a new record in the stock_sentiment_results table
+#         stock_sentiment_result = StockSentimentResults(
+#             stock_id=stock_id,
+#             final_result=final_result,
+#             time_from=datetime(2023, 3, 20, 0, 0, 0),
+#             time_to=datetime(2023, 3, 30, 0, 0, 0),
+#         )
+#         session.add(stock_sentiment_result)
+    
+#     session.commit()
+# session.commit()
--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, BigInteger, Float
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 import snscrape.modules.twitter as tw 
-from sqlalchemy import Enum
+from tables import SymbolConfig, SentimentString
 
 # create the engine
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/analysis')
@@ -17,57 +17,6 @@ session = Session()
 # create the base class for declarative models
 Base = declarative_base()
 
-# define the symbol_config table
-class SymbolConfig(Base):
-    __tablename__ = 'symbol_config'
-    id = Column(Integer, primary_key=True)
-    type = Column(Enum('stock', 'crypto', 'commodity', name='type_enum'))   
-    name = Column(String, unique=True) 
-    symbol_name = Column(String)
-    subtype = Column(String)
-    created_date = Column(DateTime, default=datetime.now)
-    updated_date = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    status = Column(Enum('enabled', 'disabled', name='status_enum'))
-    sentiment_strings = relationship("SentimentString", backref="symbol_config")
-
-# define the sentiment_strings table
-class SentimentString(Base):
-    __tablename__ = 'sentiment_strings'
-    id = Column(Integer, primary_key=True)
-    symbol_config_id = Column(Integer, ForeignKey('symbol_config.id'))
-    tweet_id = Column(BigInteger, unique=True)
-    strings = Column(String)
-    data_date = Column(DateTime)
-    created_date = Column(DateTime, default=datetime.now)
-    updated_date = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-
-# define the modeldetails table
-class ModelDetails(Base):
-    __tablename__ = 'model_details'
-    id = Column(Integer, primary_key=True)
-    model_name = Column(String)
-    model_version = Column(String, default='1.0')
-
-# define the sentiment_results table
-class SentimentResult(Base):
-    __tablename__ = 'sentiment_results'
-    id = Column(Integer, primary_key=True)
-    tweet_id = Column(BigInteger,primary_key=True)
-    symbol_config_id = Column(Integer, ForeignKey('symbol_config.id'))
-    sentiment_result = Column(String)
-    sentiment_score = Column(Integer)
-    model_name_id = Column(Integer, ForeignKey('model_details.id'))
-    created_date = Column(DateTime, default=datetime.now)
-
-# define the stock_sentiment_results table
-class StockSentimentResults(Base):
-    __tablename__ = 'stock_sentiment_results'
-    id = Column(Integer, primary_key=True)
-    stock_id = Column(Integer, ForeignKey('symbol_config.id'))
-    final_result = Column(Float)
-    time_from = Column(DateTime)
-    time_to = Column(DateTime)
-    created_date = Column(DateTime, default=datetime.now)
 
 # create the tables
 Base.metadata.create_all(engine)
